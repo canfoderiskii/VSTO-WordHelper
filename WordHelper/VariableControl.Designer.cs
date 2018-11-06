0700b773874eef5a265b35c774ca28699ed565c9
canfoderiskii <wch007@msn.com>
2018-11-06 11:25:38 +0800

    Enhancement: Variable DataGrid autofill the width of control

--- WordHelper/VariableControl.Designer.cs
+++ WordHelper/VariableControl.Designer.cs
@@ -31,11 +31,16 @@
             this.VariableContextDelete = new System.Windows.Forms.ToolStripMenuItem();
             this.VariableReloadButton = new System.Windows.Forms.Button();
             this.VariableConfirmButton = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.State = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.key = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.value = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.VariableDataGrid)).BeginInit();
             this.VariableContextMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // VariableDataGrid
@@ -46,12 +51,13 @@
             this.key,
             this.value});
             this.VariableDataGrid.ContextMenuStrip = this.VariableContextMenu;
-            this.VariableDataGrid.Location = new System.Drawing.Point(0, 42);
+            this.VariableDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.VariableDataGrid.Location = new System.Drawing.Point(0, 0);
             this.VariableDataGrid.Name = "VariableDataGrid";
             this.VariableDataGrid.RowHeadersVisible = false;
             this.VariableDataGrid.RowTemplate.Height = 23;
             this.VariableDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.VariableDataGrid.Size = new System.Drawing.Size(228, 395);
+            this.VariableDataGrid.Size = new System.Drawing.Size(229, 403);
             this.VariableDataGrid.TabIndex = 0;
             // 
             // VariableContextMenu
@@ -70,7 +76,7 @@
             // 
             // VariableReloadButton
             // 
-            this.VariableReloadButton.Location = new System.Drawing.Point(4, 8);
+            this.VariableReloadButton.Location = new System.Drawing.Point(3, 3);
             this.VariableReloadButton.Name = "VariableReloadButton";
             this.VariableReloadButton.Size = new System.Drawing.Size(46, 28);
             this.VariableReloadButton.TabIndex = 1;
@@ -80,13 +86,33 @@
             // 
             // VariableConfirmButton
             // 
-            this.VariableConfirmButton.Location = new System.Drawing.Point(56, 8);
+            this.VariableConfirmButton.Location = new System.Drawing.Point(55, 3);
             this.VariableConfirmButton.Name = "VariableConfirmButton";
             this.VariableConfirmButton.Size = new System.Drawing.Size(46, 28);
             this.VariableConfirmButton.TabIndex = 1;
             this.VariableConfirmButton.Text = "确认";
             this.VariableConfirmButton.UseVisualStyleBackColor = true;
             this.VariableConfirmButton.Click += new System.EventHandler(this.VariableConfirmButton_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.VariableReloadButton);
+            this.splitContainer1.Panel1.Controls.Add(this.VariableConfirmButton);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.VariableDataGrid);
+            this.splitContainer1.Size = new System.Drawing.Size(229, 440);
+            this.splitContainer1.SplitterDistance = 33;
+            this.splitContainer1.TabIndex = 2;
             // 
             // State
             // 
@@ -99,26 +125,29 @@
             // 
             // key
             // 
+            this.key.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.key.HeaderText = "变量名";
             this.key.Name = "key";
             // 
             // value
             // 
+            this.value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.value.HeaderText = "变量值";
             this.value.Name = "value";
-            this.value.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
             // VariableControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.VariableConfirmButton);
-            this.Controls.Add(this.VariableReloadButton);
-            this.Controls.Add(this.VariableDataGrid);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "VariableControl";
             this.Size = new System.Drawing.Size(229, 440);
             ((System.ComponentModel.ISupportInitialize)(this.VariableDataGrid)).EndInit();
             this.VariableContextMenu.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -130,6 +159,7 @@
         private System.Windows.Forms.Button VariableConfirmButton;
         private System.Windows.Forms.ContextMenuStrip VariableContextMenu;
         private System.Windows.Forms.ToolStripMenuItem VariableContextDelete;
+        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.DataGridViewTextBoxColumn State;
         private System.Windows.Forms.DataGridViewTextBoxColumn key;
         private System.Windows.Forms.DataGridViewTextBoxColumn value;
